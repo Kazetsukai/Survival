c05836488408fbfc7d569a3979fdee85932b87aa
joejimufu <righteouslyalgebraic@gmail.com>
2014-10-30 13:10:26 +1300

    Experimented with Unity animation controllers
    Add Animation Controller to placeholder model, along with animation
    behaviour script. Model now runs when the player moves, and idles when
    the player stops.

---
+++ Unity/Assets/Test Assets/Scripts/limbs_rig_test_anim_behaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class limbs_rig_test_anim_behaviour : MonoBehaviour 
+{
+	int walkingHash = Animator.StringToHash("Base Layer.run");
+
+	Animator anim;
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update () 
+	{
+		//Rough code to change animation state for debug legs. Makes them run if the player presses forward button.
+		float move = Input.GetAxis ("Vertical");
+		if (move != 0) 
+		{
+			anim.SetBool("run", true);
+		}
+		else
+		{
+			anim.SetBool("run", false);
+		}
+	}
+}
