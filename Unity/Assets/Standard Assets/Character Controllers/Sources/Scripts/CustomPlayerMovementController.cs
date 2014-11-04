708db9f8a1b2df46e49d1ce60be92305a8e3f886
DavidKarlEnlighten <david.karl@enlighten.co.nz>
2014-11-04 16:49:05 +1300

    Acceleration applied in slope direction
    Updated the script to adjust the acceleration vector to match the slope
    that the player is walking on. More work needed to scale the player's
    speed based on the slope angle.

--- Unity/Assets/Standard Assets/Character Controllers/Sources/Scripts/CustomPlayerMovementController.cs
+++ Unity/Assets/Standard Assets/Character Controllers/Sources/Scripts/CustomPlayerMovementController.cs
@@ -9,6 +9,8 @@ public class CustomPlayerMovementController : MonoBehaviour {
 	//public float backwardSpeedFactor = 0.3F;
 	public float acceleration = 100F;
 	Rigidbody rb;
+	bool isGrounded = true;
+	RaycastHit hit;
 
 	// Use this for initialization
 	void Start () {
@@ -20,12 +22,41 @@ public class CustomPlayerMovementController : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
-		Vector3 movementVector = transform.forward * (int)Input.GetAxis ("Vertical") + transform.right * (int)Input.GetAxis ("Horizontal");
-		Vector3 targetVector = movementVector.normalized * jogSpeed * (Input.GetAxis ("Vertical") > 0 ? (Input.GetAxis ("Walk") == 1F ? walkSpeedFactor : 1 * Input.GetAxis("Sprint") == 1 ? sprintSpeedFactor : 1) : walkSpeedFactor);
-		Vector3 currentVelocity = rigidbody.velocity;
-		currentVelocity.y = 0F;
-		Vector3 accelerationVector = (targetVector - currentVelocity);
-		accelerationVector = Vector3.ClampMagnitude (accelerationVector, acceleration);
-		rb.AddForce (accelerationVector);
+		isGrounded = GroundedTest ();
+		if (isGrounded) {
+			Vector3 movementVector = transform.forward * (int)Input.GetAxis ("Vertical") + transform.right * (int)Input.GetAxis ("Horizontal");
+			Vector3 targetVector = movementVector.normalized * jogSpeed * (Input.GetAxis ("Vertical") > 0 ? (Input.GetAxis ("Walk") == 1F ? walkSpeedFactor : 1 * Input.GetAxis ("Sprint") == 1 ? sprintSpeedFactor : 1) : walkSpeedFactor);
+			Vector3 currentVelocity = rigidbody.velocity;
+			currentVelocity.y = 0F;
+			Vector3 accelerationVector = (targetVector - currentVelocity);
+			accelerationVector = Vector3.ClampMagnitude (accelerationVector, acceleration);
+
+			float theta = Vector3.Angle(hit.normal, Vector3.Cross (accelerationVector, Vector3.Cross(hit.normal, accelerationVector)));
+			theta *= Mathf.Deg2Rad;
+
+			Vector3 v = accelerationVector;
+			Vector3 k = -transform.right;
+
+			Vector3 vRot = v * Mathf.Cos(theta) + (Vector3.Cross(k, v)) * Mathf.Sin(theta) + k * (Vector3.Dot(k, v)) * (1 - Mathf.Cos(theta));
+			rb.AddForce (vRot);
+
+			//Debug.DrawRay (transform.position, accelerationVector, Color.red, 0, true);
+			//Debug.DrawRay (transform.position, currentVelocity, Color.green, 0, true);
+			//Debug.DrawRay (hit.point, hit.normal, Color.blue, 0, true);
+			//Debug.DrawRay (hit.point, Vector3.Cross(hit.normal, accelerationVector), Color.cyan, 0, true);
+			//Debug.DrawRay (hit.point, Vector3.Cross (accelerationVector, Vector3.Cross(hit.normal, accelerationVector)), Color.magenta, 0, true);
+			//Debug.Log(Vector3.Angle(hit.normal, Vector3.Cross (accelerationVector, Vector3.Cross(hit.normal, accelerationVector))));
+			//Debug.DrawRay(hit.point, vRot, Color.red, 0, true);
+
+		}
+	}
+
+	public bool IsGrounded() {
+		return isGrounded;
+	}
+
+	private bool GroundedTest() {
+		int layerMask = 1 << 8;
+		return Physics.Raycast (transform.position, Vector3.down, out hit, 2, layerMask);
 	}
 }
