10a40d8ccca391a159e37b49736b8b8385937cc5
Daniel Tebbutt <moeruze.burning@gmail.com>
2014-11-09 23:27:01 +1300

    Revert "Deleted file"
    This reverts commit ada0cf5700a156a6381147d702d45811b3c906fa.

---
+++ Unity/Assets/Scripts/Terrain.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Terrain : MonoBehaviour {
+
+	float slopeStumbleConstant = 800F;
+	float slopeStumbleCoefficient = 0.045F;
+	float slopeStumbleExponent = 3F;
+	public string terrainType;
+
+	// Use this for initialization
+	void Start () {
+		switch(terrainType) {
+		case "Gravel":
+			slopeStumbleConstant = 800F;
+			slopeStumbleCoefficient = 0.045F;
+			slopeStumbleExponent = 3F;
+			break;
+		case "Grass":
+			slopeStumbleConstant = 900F;
+			slopeStumbleCoefficient = 0.007F;
+			slopeStumbleExponent = 3F;
+			break;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public float SlopeStumbleConstant() {
+		return slopeStumbleConstant;
+	}
+
+	public float SlopeStumbleCoefficient() {
+		return slopeStumbleCoefficient;
+	}
+
+	public float SlopeStumbleExponent() {
+		return slopeStumbleExponent;
+	}
+
+}
