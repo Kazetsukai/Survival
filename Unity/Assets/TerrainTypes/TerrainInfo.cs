6d8b79bebeb026ebab9e1ae96a74890d1387f20b
Daniel Tebbutt <moeruze.burning@gmail.com>
2014-11-09 23:25:40 +1300

    Shuffling files and adding terrain info

---
+++ Unity/Assets/TerrainTypes/TerrainInfo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainInfo : MonoBehaviour {
+
+	public AudioClip[] FootstepSounds;
+	
+	public float SlopeStumbleConstant;
+	public float SlopeStumbleCoefficient;
+	public float SlopeStumbleExponent;
+}
