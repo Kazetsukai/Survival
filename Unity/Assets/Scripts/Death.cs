8399b6bdecbf3b7475ba4a934e263cf6a4e6842b
Daniel Tebbutt <moeruze.burning@gmail.com>
2014-11-13 20:51:00 +1300

    Fixed ragdolling, now recursively sets position and rotation properly
    This means the ragdoll is setting ALL bones to the right location and
    orientation. Only thing left is velocity.

--- Unity/Assets/Scripts/Death.cs
+++ Unity/Assets/Scripts/Death.cs
@@ -30,29 +30,35 @@ public class Death : MonoBehaviour {
 		rigidbody.useGravity = false;
 		rigidbody.freezeRotation = true;
 		
-		CharacterAvatarObject.SetActive(false);
 		Ragdoll.SetActive(true);
 		
-		Ragdoll.transform.position = CharacterAvatarObject.transform.position;
-		
 		// Set all bones to same position as animated character
-		foreach (Transform child in CharacterAvatarObject.transform)
-		{
-			foreach (Transform ragChild in Ragdoll.transform)
-			{
-				if (ragChild.gameObject.name == child.gameObject.name)
-				{
-					ragChild.position = child.position;
-					break;
-				}
-			}
-		}
-
+		CopyPosition(CharacterAvatarObject.transform, Ragdoll.transform);
+		
+		CharacterAvatarObject.SetActive(false);
+		
 		DeathCamera.GetComponent<Camera> ().enabled = true;
 		DeathCamera.GetComponent<AudioListener> ().enabled = true;
 		
 		foreach (var behaviour in GetComponents<MonoBehaviour>()) {
 			behaviour.enabled = false;
+		}
+	}
+	
+	public void CopyPosition(Transform fromTransform, Transform toTransform)
+	{
+		toTransform.position = fromTransform.position;
+		toTransform.rotation = fromTransform.rotation;
+		
+		foreach (Transform child in fromTransform)
+		{
+			foreach (Transform ragChild in toTransform)
+			{
+				if (ragChild.gameObject.name == child.gameObject.name)
+				{
+					CopyPosition (child, ragChild);
+				}
+			}
 		}
 	}
 }
