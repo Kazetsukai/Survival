96ee81a09cb7addcf783d8e616977f09c1ff2a82
Daniel Tebbutt <moeruze.burning@gmail.com>
2014-11-15 13:24:28 +1300

    Made an end goal that flings you into the air.

---
+++ Unity/Assets/WinScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.name == "Player Controller")
+		{
+			other.gameObject.SendMessage("Die");
+			GameObject.Find ("ragdoll").rigidbody.AddForce (Vector3.up * 500, ForceMode.VelocityChange);
+		}
+	}
+}
