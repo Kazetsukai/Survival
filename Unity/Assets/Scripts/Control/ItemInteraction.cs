a974fc4fd66b2817afd9914eea099a79f45905e8
Daniel Tebbutt <daniel.tebbutt@enlighten.co.nz>
2014-12-19 12:25:35 +1300

    Midas mode
    Starting on item interaction: cast a ray out from the camera and make a
    bubble where that ray hits. Anything within this bubble that is
    interactable will be turned yellow. So far no way to actually interact
    with objects yet though (will be mouse click at first) and things don't
    change back to their original colour.

---
+++ Unity/Assets/Scripts/Control/ItemInteraction.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class ItemInteraction : MonoBehaviour {
+
+	public float MaxInteractDistance = 3.0f;
+	public float InteractBubbleSize = 0.8f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit pointHit;
+		Ray ray = new Ray (transform.position, transform.forward);
+
+		if (Physics.Raycast (ray, out pointHit, MaxInteractDistance)) {
+			
+			var detectedObjects = Physics.OverlapSphere(pointHit.point, InteractBubbleSize);
+
+			var interactableObjects = detectedObjects.Where(o => o.GetComponents<MonoBehaviour>().Any(m => m is IInteraction));
+
+			foreach (var obj in interactableObjects)
+			{
+				obj.renderer.material.color = Color.yellow;
+			}
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		RaycastHit pointHit;
+		Ray ray = new Ray (transform.position, transform.forward);
+		Gizmos.DrawRay (ray);
+
+		if (Physics.Raycast (ray, out pointHit, MaxInteractDistance)) {
+			
+			var detectedObjects = Physics.OverlapSphere(pointHit.point, InteractBubbleSize);
+			Gizmos.DrawWireSphere (pointHit.point, InteractBubbleSize);
+			
+		}
+
+	}
+}
---
+++ Unity/Assets/Scripts/GameLogic/Items/NullInteraction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NullInteraction : MonoBehaviour, IInteraction {
+
+	public void Interact ()
+	{
+
+	}
+
+}
