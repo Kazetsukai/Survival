d5225b83109aa81b6e7d034076c6ff2e49b42f62
Daniel Tebbutt <moeruze.burning@gmail.com>
2014-12-22 10:11:04 +1300

    Selection highlighting on selected objects
    Move close to and aim at an object to select it. This system should
    handle small items on the ground reasonably well as well. Uses raycast
    first, and then selection bubble if nothing is hit by the raycast.

--- Unity/Assets/Scripts/Control/ItemInteraction.cs
+++ Unity/Assets/Scripts/Control/ItemInteraction.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Linq;
+using System.Collections.Generic;
 
 public class ItemInteraction : MonoBehaviour {
 
 	public float MaxInteractDistance = 3.0f;
 	public float InteractBubbleSize = 0.8f;
+    public Material SelectionMaterial;
+    public GameObject SelectedObject;
+
+    private GameObject _selectionObject;
 
 	// Use this for initialization
 	void Start () {
-	
+        _selectionObject = new GameObject();
+        _selectionObject.AddComponent<MeshRenderer>();
+        _selectionObject.AddComponent<MeshFilter>();
+        _selectionObject.renderer.material = SelectionMaterial;
 	}
 	
 	// Update is called once per frame
@@ -17,18 +25,46 @@ public class ItemInteraction : MonoBehaviour {
 		RaycastHit pointHit;
 		Ray ray = new Ray (transform.position, transform.forward);
 
+        Collider objectToSelect = null;
+
 		if (Physics.Raycast (ray, out pointHit, MaxInteractDistance)) {
-			
-			var detectedObjects = Physics.OverlapSphere(pointHit.point, InteractBubbleSize);
-
-			var interactableObjects = detectedObjects.Where(o => o.GetComponents<MonoBehaviour>().Any(m => m is IInteraction));
-
-			foreach (var obj in interactableObjects)
-			{
-				obj.renderer.material.color = Color.yellow;
-			}
+            if (pointHit.collider.gameObject.GetComponents<MonoBehaviour>().Any(m => m is IInteraction))
+                objectToSelect = pointHit.collider;
+            else
+            {
+                var detectedObjects = Physics.OverlapSphere(pointHit.point, InteractBubbleSize);
+                var interactableObjects = detectedObjects.Where(o => o.GetComponents<MonoBehaviour>().Any(m => m is IInteraction));
+                objectToSelect = interactableObjects.OrderBy(c => (pointHit.point - c.transform.position).magnitude).FirstOrDefault();
+            }
 		}
+
+        SelectedObject = objectToSelect == null ? null : objectToSelect.gameObject;
+
+        ShowSelection(SelectedObject);
 	}
+
+    private void ShowSelection(GameObject selectedObject)
+    {
+        MeshFilter meshFilter = null;
+
+        if (selectedObject != null && selectedObject.renderer != null)
+            meshFilter = selectedObject.GetComponent<MeshFilter>();
+
+        if (meshFilter != null && meshFilter.mesh != null)
+        {
+            _selectionObject.renderer.enabled = true;
+
+            _selectionObject.transform.position = selectedObject.transform.position;
+            _selectionObject.transform.rotation = selectedObject.transform.rotation;
+            _selectionObject.transform.localScale = selectedObject.transform.lossyScale * 1.01f;
+
+            _selectionObject.GetComponent<MeshFilter>().mesh = meshFilter.mesh;
+        }
+        else
+        {
+            _selectionObject.renderer.enabled = false;
+        }
+    }
 
 	void OnDrawGizmos()
 	{
