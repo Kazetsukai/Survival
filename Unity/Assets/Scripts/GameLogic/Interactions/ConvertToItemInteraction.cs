f27a93a3e48bfeea52dbad014cbcec20a71d9df0
joejimufu <righteouslyalgebraic@gmail.com>
2015-01-09 11:05:49 +1300

    Added ConvertToItemInteraction.cs
    This script use IInteraction, and causes an object to add a rigidbody
    and a CarryableItem script to itself when interacted with. The script
    also removes itself from the object to prevent recursion.

---
+++ Unity/Assets/Scripts/GameLogic/Interactions/ConvertToItemInteraction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Converts a GameObject to a new GameObject with RigidBody and CarryableItem script.
+/// </summary>
+public class ConvertToItemInteraction : MonoBehaviour, IInteraction 
+{
+	public void Interact()
+	{
+		//Spawn copy of this object in same place with same rotation
+		this.gameObject.AddComponent<Rigidbody> ();
+		this.gameObject.AddComponent<CarryableItem> ();
+		this.name = this.name + "_asItem";
+		Destroy (this);				//Remove this script
+	}
+}
